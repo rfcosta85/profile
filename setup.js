3de89f975ceddac5d7426bd25c9e7a376b1d91f7
Rodrigo Costa <rfcosta85@gmail.com>
2020-11-16 21:09:08 -0300

    Ajustes segunda tela

---
+++ setup.js
@@ -0,0 +1,15 @@
+$('#owl-slider-languages').owlCarousel({
+    
+    navigation: false,
+    pagination: true,
+    itemsCustom: [
+
+        [0, 1],
+        [1500, 1],
+        [1500, 1],
+        
+    ],
+    
+    navigationText: false
+
+});
